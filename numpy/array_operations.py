441ac75057990efdabf8d0d9239c134d6d3b3089
emiryalcinkaya <emirylk06@gmail.com>
2026-06-06 14:40:22 +0200

    Add NumPy array operations example

---
+++ numpy/array_operations.py
@@ -0,0 +1,13 @@
+"""
+Perform basic operations on NumPy arrays.
+"""
+
+import numpy as np
+
+numbers = np.array([10, 20, 30, 40, 50])
+
+print("Original:", numbers)
+print("Add 10:", numbers + 10)
+print("Subtract 5:", numbers - 5)
+print("Multiply by 2:", numbers * 2)
+print("Divide by 2:", numbers / 2)
