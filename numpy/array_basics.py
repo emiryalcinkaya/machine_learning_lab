4388c9238dc9570961a0cb9a56f0c4c69072f9b6
emiryalcinkaya <emirylk06@gmail.com>
2026-06-06 14:35:16 +0200

    Add NumPy array basics example

---
+++ numpy/array_basics.py
@@ -0,0 +1,12 @@
+"""
+Create and access NumPy arrays.
+"""
+
+import numpy as np
+
+numbers = np.array([10, 20, 30, 40, 50])
+
+print("Array:", numbers)
+print("First Element:", numbers[0])
+print("Last Element:", numbers[-1])
+print("Length:", len(numbers))
